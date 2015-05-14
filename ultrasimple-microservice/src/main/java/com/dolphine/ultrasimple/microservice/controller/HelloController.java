0aaac3265aafec66fd1487b18ea1d49a621e8da8
Tiago Dolphine <tiago.dolphine@ifood.com.br>
2015-05-14 14:00:08 -0300

    json

--- ultrasimple-microservice/src/main/java/com/dolphine/ultrasimple/microservice/controller/HelloController.java
+++ ultrasimple-microservice/src/main/java/com/dolphine/ultrasimple/microservice/controller/HelloController.java
@@ -1,37 +1,32 @@
-/*
- * Copyright 2012-2013 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.dolphine.ultrasimple.microservice.controller;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.util.MimeTypeUtils;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@Controller
+@ComponentScan
+@RestController
 public class HelloController {
 
-	@RequestMapping(value="/",produces={MimeTypeUtils.TEXT_PLAIN_VALUE})
-	@ResponseBody
+	@RequestMapping(value="/",method=RequestMethod.GET)
 	public String helloWorld(@RequestParam String name) {
 		return "Olá " + name;
+	}
+	
+	@RequestMapping(value="/",method=RequestMethod.GET, produces={MediaType.APPLICATION_JSON_VALUE})
+	public Map<String,String> helloWorldJson(@RequestParam String name) {
+		Map<String,String> obj=new LinkedHashMap<String,String>();
+		obj.put("message", "Olá "+name);
+		return obj;
 	}
 	
 	public static void main(String... args) throws Exception {
